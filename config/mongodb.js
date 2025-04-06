49169aa66b973a66e34a7c8ddb0425ca751d5c60
Divya Chhabra <divichhabra49@gmail.com>
2025-04-06 13:22:26 +0530

    //

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,7 +3,7 @@ import mongoose, { connect } from "mongoose";
 const connectDB = async ()=>{
 
     mongoose.connection.on('connected',()=> console.log("DATABASE CONNECTED"));
-    await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
+    await mongoose.connect(`${process.env.MONGODB_URI}`);
 
 };
 
